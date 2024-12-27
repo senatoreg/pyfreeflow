548beadf1108d0bf670363ce0b519a050c2dea34
Giovanni Senatore <giovanni.senatore at example.com>
2024-12-27 14:03:58 +0100

    Add main module import __init__.py

---
+++ freeflow/__init__.py
@@ -0,0 +1,34 @@
+import importlib
+import logging
+import freeflow.ext
+import freeflow.pipeline
+from sys import version_info
+
+
+handler = logging.StreamHandler()
+if version_info.major > 3 or (version_info.major == 3 and
+                              version_info.minor > 11):
+    formatter = logging.Formatter('[%(asctime)s] %(name)s - TaskName[%(taskName)s] - %(levelname)s - %(message)s')
+else:
+    formatter = logging.Formatter('[%(asctime)s] %(name)s - %(levelname)s - %(message)s')
+
+handler.setFormatter(formatter)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
+logger.propagate = True
+logger.addHandler(handler)
+
+
+def load_extension(ext_name):
+    logger.info("Loading extension: %s", ext_name)
+    importlib.import_module(ext_name)
+    logger.info("Loaded extension: %s", ext_name)
+
+
+def set_loglevel(level):
+    logger.setLevel(level)
+
+
+def get_logformat():
+    return formatter._fmt
