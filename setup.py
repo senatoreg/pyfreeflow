0e156b689a8478ec13cf03ef7df679fef6fba38e
Giovanni Senatore <giovanni.senatore at example.com>
2025-01-06 16:13:18 +0100

    Add package builder

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+with open("requirements.txt", "r") as f:
+    required_packages = [line.strip() for line in f if line.strip() and not line.startswith("#")]
+print(required_packages)
+
+setup(
+    name="freeflow",
+    version="0.1.0",
+    author="Giovanni Senatore",
+    author_email="",
+    description="Async service toolchain",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/senatoreg/pyfreeflow",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=required_packages,
+    scripts= [
+        "scripts/freeflow-cli.py",
+    ],
+    license="AGPL-3.0-or-later",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+)
+
