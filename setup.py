47e444bb439a127988479d80732b3bfd955545e9
Giovanni Senatore <giovanni.senatore at example.com>
2025-01-18 16:19:49 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ print(required_packages)
 
 setup(
     name="pyfreeflow",
-    version="0.1.3",
+    version="0.1.4",
     author="Giovanni Senatore",
     author_email="",
     description="Async service toolchain",
